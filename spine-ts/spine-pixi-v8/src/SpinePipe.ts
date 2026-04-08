masquepublishing/spine-runtimes

commit a5ac6e4dd55305d440a9d612e350b6af50c53df7

[ts] Port latest libgdx timeline, sequence, and follow-up fixes.

spine-ts/spine-pixi-v8/src/SpinePipe.ts
@@ -216,8 +216,10 @@ export class SpinePipe implements RenderPipe<Spine> {
 
 				if (!cacheData.skipRender) {
 					const batchableSpineSlot = gpuSpine.slotBatches[cacheData.id];
-					// we didn't figure out why batchableSpineSlot might be undefined: https://github.com/EsotericSoftware/spine-runtimes/issues/2991
-					batchableSpineSlot?._batcher?.updateElement(batchableSpineSlot);
+					if (batchableSpineSlot) {
+						batchableSpineSlot.uvs = cacheData.uvs;
+						batchableSpineSlot._batcher?.updateElement(batchableSpineSlot);
+					}
 				}
 			}
 		}

spine-ts/index.html
@@ -47,6 +47,7 @@
         <li><a href="/spine-pixi-v8/example/bounds.html">Bounds</a> - (<a href="/spine-pixi-v7/example/bounds.html">v7</a>)</li>
         <li><a href="/spine-pixi-v8/example/inline-loading.html">Inline loading</a> - (<a href="/spine-pixi-v7/example/inline-loading.html">v7</a>)</li>
         <li><a href="/spine-pixi-v8/example/bunnymark.html?count=500&renderer=webgpu">Bunny Mark</a> - (<a href="/spine-pixi-v7/example/bunnymark.html?count=500">v7</a>)</li>
+        <li><a href="/spine-pixi-v8/example/dragon.html">Dragon</a> - (<a href="/spine-pixi-v7/example/dragon.html">v7</a>)</li>
       </ul>
       <li>Phaser</li>
       <ul>
@@ -142,6 +143,7 @@
       <ul>
         <li><a href="/spine-webgl/example">Example</a></li>
         <li><a href="/spine-webgl/example/barebones.html">Barebones</a></li>
+        <li><a href="/spine-webgl/example/dragon.html">Dragon</a></li>
         <li><a href="/spine-webgl/example/physics.html">Physics</a></li>
         <li><a href="/spine-webgl/example/physics2.html">Physics II</a></li>
         <li><a href="/spine-webgl/example/physics3.html">Physics III</a></li>

spine-ts/spine-canvas/src/SkeletonRenderer.ts
@@ -27,7 +27,7 @@
  * THE SPINE RUNTIMES, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************/
 
-import { type BlendMode, Color, MeshAttachment, RegionAttachment, type Skeleton, type Slot, type TextureRegion, Utils } from "@esotericsoftware/spine-core";
+import { type BlendMode, Color, MeshAttachment, type NumberArrayLike, RegionAttachment, type Skeleton, type Slot, type TextureRegion, Utils } from "@esotericsoftware/spine-core";
 import type { CanvasTexture } from "./CanvasTexture.js";
 
 const worldVertices = Utils.newFloatArray(8);
@@ -68,10 +68,14 @@ export class SkeletonRenderer {
 			const pose = slot.applied;
 			const attachment = pose.attachment;
 			if (!(attachment instanceof RegionAttachment)) continue;
-			attachment.computeWorldVertices(slot, worldVertices, 0, 2);
-			const region: TextureRegion = <TextureRegion>attachment.region;
 
-			const image: HTMLImageElement = (<CanvasTexture>region.texture).getImage() as HTMLImageElement;
+			const sequence = attachment.sequence;
+			const sequenceIndex = sequence.resolveIndex(pose);
+			attachment.computeWorldVertices(slot, attachment.getOffsets(pose), worldVertices, 0, 2);
+
+			const region = sequence.regions[sequenceIndex] as TextureRegion;
+
+			const image: HTMLImageElement = region.texture.getImage() as HTMLImageElement;
 
 			const slotColor = pose.color;
 			const regionColor = attachment.color;
@@ -83,7 +87,8 @@ export class SkeletonRenderer {
 			ctx.save();
 			const boneApplied = bone.applied;
 			ctx.transform(boneApplied.a, boneApplied.c, boneApplied.b, boneApplied.d, boneApplied.worldX, boneApplied.worldY);
-			ctx.translate(attachment.offset[0], attachment.offset[1]);
+			const offsets = attachment.getOffsets(pose);
+			ctx.translate(offsets[0], offsets[1]);
 			ctx.rotate(attachment.rotation * Math.PI / 180);
 
 			const atlasScale = attachment.width / region.originalWidth;
@@ -91,7 +96,7 @@ export class SkeletonRenderer {
 
 			let w = region.width, h = region.height;
 			ctx.translate(w / 2, h / 2);
-			if (attachment.region?.degrees === 90) {
+			if (region.degrees === 90) {
 				const t = w;
 				w = h;
 				h = t;
@@ -124,15 +129,25 @@ export class SkeletonRenderer {
 
 			let texture: HTMLImageElement;
 			if (attachment instanceof RegionAttachment) {
-				const regionAttachment = <RegionAttachment>attachment;
-				vertices = this.computeRegionVertices(slot, regionAttachment, false);
+				const sequence = attachment.sequence;
+				const sequenceIndex = sequence.resolveIndex(pose);
+
+				const uvs = sequence.getUVs(sequenceIndex);
+				const offsets = attachment.getOffsets(pose);
+
+				vertices = this.computeRegionVertices(slot, attachment, offsets, uvs, false);
 				triangles = SkeletonRenderer.QUAD_TRIANGLES;
-				texture = (<CanvasTexture>regionAttachment.region?.texture).getImage() as HTMLImageElement;
+
+				texture = (sequence.regions[sequenceIndex]?.texture as CanvasTexture).getImage();
 			} else if (attachment instanceof MeshAttachment) {
-				const mesh = <MeshAttachment>attachment;
-				vertices = this.computeMeshVertices(slot, mesh, false);
-				triangles = mesh.triangles;
-				texture = (<CanvasTexture>mesh.region?.texture).getImage() as HTMLImageElement;
+				const sequence = attachment.sequence;
+				const sequenceIndex = sequence.resolveIndex(pose);
+
+				const uvs = sequence.getUVs(sequenceIndex);
+				vertices = this.computeMeshVertices(slot, attachment, uvs, false);
+				triangles = attachment.triangles;
+
+				texture = (sequence.regions[sequenceIndex]?.texture as CanvasTexture).getImage();
 			} else
 				continue;
 
@@ -226,7 +241,7 @@ export class SkeletonRenderer {
 		ctx.restore();
 	}
 
-	private computeRegionVertices (slot: Slot, region: RegionAttachment, pma: boolean) {
+	private computeRegionVertices (slot: Slot, region: RegionAttachment, offsets: NumberArrayLike, uvs: NumberArrayLike, pma: boolean) {
 		const skeletonColor = slot.skeleton.color;
 		const slotColor = slot.applied.color;
 		const regionColor = region.color;
@@ -238,10 +253,9 @@ export class SkeletonRenderer {
 			skeletonColor.b * slotColor.b * regionColor.b * multiplier,
 			alpha);
 
-		region.computeWorldVertices(slot, this.vertices, 0, SkeletonRenderer.VERTEX_SIZE);
+		region.computeWorldVertices(slot, offsets, this.vertices, 0, SkeletonRenderer.VERTEX_SIZE);
 
 		const vertices = this.vertices;
-		const uvs = region.uvs;
 
 		vertices[RegionAttachment.C1R] = color.r;
 		vertices[RegionAttachment.C1G] = color.g;
@@ -274,7 +288,7 @@ export class SkeletonRenderer {
 		return vertices;
 	}
 
-	private computeMeshVertices (slot: Slot, mesh: MeshAttachment, pma: boolean) {
+	private computeMeshVertices (slot: Slot, mesh: MeshAttachment, uvs: NumberArrayLike, pma: boolean) {
 		const skeleton = slot.skeleton;
 		const skeletonColor = skeleton.color;
 		const slotColor = slot.applied.color;
@@ -292,7 +306,7 @@ export class SkeletonRenderer {
 		if (vertices.length < mesh.worldVerticesLength) this.vertices = vertices = Utils.newFloatArray(mesh.worldVerticesLength);
 		mesh.computeWorldVertices(skeleton, slot, 0, mesh.worldVerticesLength, vertices, 0, SkeletonRenderer.VERTEX_SIZE);
 
-		const uvs = mesh.uvs;
+
 		for (let i = 0, u = 0, v = 2; i < vertexCount; i++) {
 			vertices[v++] = color.r;
 			vertices[v++] = color.g;

spine-ts/spine-core/src/AnimationState.ts
@@ -198,7 +198,7 @@ export class AnimationState {
 					Utils.webkit602BugfixHelper(alpha, blend);
 					const timeline = timelines[ii];
 					if (timeline instanceof AttachmentTimeline)
-						this.applyAttachmentTimeline(timeline, skeleton, applyTime, blend, attachments);
+						this.applyAttachmentTimeline(timeline, skeleton, applyTime, blend, false, attachments);
 					else
 						timeline.apply(skeleton, animationLast, applyTime, applyEvents, alpha, blend, MixDirection.in, false);
 				}
@@ -215,7 +215,7 @@ export class AnimationState {
 					if (!shortestRotation && timeline instanceof RotateTimeline) {
 						this.applyRotateTimeline(timeline, skeleton, applyTime, alpha, timelineBlend, current.timelinesRotation, ii << 1, firstFrame);
 					} else if (timeline instanceof AttachmentTimeline) {
-						this.applyAttachmentTimeline(timeline, skeleton, applyTime, blend, attachments);
+						this.applyAttachmentTimeline(timeline, skeleton, applyTime, blend, false, attachments);
 					} else {
 						// This fixes the WebKit 602 specific issue described at https://esotericsoftware.com/forum/d/10109-ios-10-disappearing-graphics
 						Utils.webkit602BugfixHelper(alpha, blend);
@@ -286,7 +286,6 @@ export class AnimationState {
 			from.totalAlpha = 0;
 			for (let i = 0; i < timelineCount; i++) {
 				const timeline = timelines[i];
-				let direction = MixDirection.out;
 				let timelineBlend: MixBlend;
 				let alpha = 0;
 				switch (timelineMode[i]) {
@@ -319,8 +318,9 @@ export class AnimationState {
 				if (!shortestRotation && timeline instanceof RotateTimeline)
 					this.applyRotateTimeline(timeline, skeleton, applyTime, alpha, timelineBlend, from.timelinesRotation, i << 1, firstFrame);
 				else if (timeline instanceof AttachmentTimeline)
-					this.applyAttachmentTimeline(timeline, skeleton, applyTime, timelineBlend, attachments && alpha >= from.alphaAttachmentThreshold);
+					this.applyAttachmentTimeline(timeline, skeleton, applyTime, timelineBlend, true, attachments && alpha >= from.alphaAttachmentThreshold);
 				else {
+					let direction = MixDirection.out;
 					// This fixes the WebKit 602 specific issue described at https://esotericsoftware.com/forum/d/10109-ios-10-disappearing-graphics
 					Utils.webkit602BugfixHelper(alpha, blend);
 					if (drawOrder && timeline instanceof DrawOrderTimeline && timelineBlend === MixBlend.setup)
@@ -338,11 +338,13 @@ export class AnimationState {
 		return mix;
 	}
 
-	applyAttachmentTimeline (timeline: AttachmentTimeline, skeleton: Skeleton, time: number, blend: MixBlend, attachments: boolean) {
+	applyAttachmentTimeline (timeline: AttachmentTimeline, skeleton: Skeleton, time: number, blend: MixBlend, out: boolean, attachments: boolean) {
 		const slot = skeleton.slots[timeline.slotIndex];
 		if (!slot.bone.active) return;
 
-		if (time < timeline.frames[0]) { // Time is before first frame.
+		if (out) {
+			if (blend === MixBlend.setup) this.setAttachment(skeleton, slot, slot.data.attachmentName, attachments);
+		} else if (time < timeline.frames[0]) { // Time is before first frame.
 			if (blend === MixBlend.setup || blend === MixBlend.first)
 				this.setAttachment(skeleton, slot, slot.data.attachmentName, attachments);
 		} else
@@ -965,7 +967,8 @@ export class TrackEntry {
 	 * to 1, which overwrites the skeleton's current pose with this animation.
 	 *
 	 * Typically track 0 is used to completely pose the skeleton, then alpha is used on higher tracks. It doesn't make sense to
-	 * use alpha on track 0 if the skeleton pose is from the last frame render. */
+	 * use alpha on track 0 if the skeleton pose is from the last frame render.
+	 * @see alphaAttachmentThreshold */
 	alpha: number = 0;
 
 	/** Seconds from 0 to the {@link #getMixDuration()} when mixing from the previous animation to this animation. May be

spine-ts/spine-core/src/AtlasAttachmentLoader.ts
@@ -51,40 +51,27 @@ export class AtlasAttachmentLoader implements AttachmentLoader {
 		this.allowMissingRegions = allowMissingRegions;
 	}
 
-	loadSequence (name: string, basePath: string, sequence: Sequence) {
+	protected findRegions (name: string, basePath: string, sequence: Sequence) {
 		const regions = sequence.regions;
-		for (let i = 0, n = regions.length; i < n; i++) {
-			const path = sequence.getPath(basePath, i);
-			regions[i] = this.atlas.findRegion(path);
-			if (regions[i] == null && !this.allowMissingRegions)
-				throw new Error(`Region not found in atlas: ${path} (sequence: ${name})`);
-		}
+		for (let i = 0, n = regions.length; i < n; i++)
+			regions[i] = this.findRegion(name, sequence.getPath(basePath, i));
+	}
+
+	protected findRegion (name: string, path: string) {
+		const region = this.atlas.findRegion(path);
+		if (!region && !this.allowMissingRegions)
+			throw new Error(`Region not found in atlas: ${path} (attachment: ${name})`);
+		return region;
 	}
 
 	newRegionAttachment (skin: Skin, name: string, path: string, sequence: Sequence): RegionAttachment {
-		const attachment = new RegionAttachment(name, path);
-		if (sequence != null) {
-			this.loadSequence(name, path, sequence);
-		} else {
-			const region = this.atlas.findRegion(path);
-			if (region == null && !this.allowMissingRegions)
-				throw new Error(`Region not found in atlas: ${path} (region attachment: ${name})`);
-			attachment.region = region;
-		}
-		return attachment;
+		this.findRegions(name, path, sequence);
+		return new RegionAttachment(name, sequence);
 	}
 
 	newMeshAttachment (skin: Skin, name: string, path: string, sequence: Sequence): MeshAttachment {
-		const attachment = new MeshAttachment(name, path);
-		if (sequence != null) {
-			this.loadSequence(name, path, sequence);
-		} else {
-			const region = this.atlas.findRegion(path);
-			if (region == null && !this.allowMissingRegions)
-				throw new Error(`Region not found in atlas: ${path} (mesh attachment: ${name})`);
-			attachment.region = region;
-		}
-		return attachment;
+		this.findRegions(name, path, sequence);
+		return new MeshAttachment(name, sequence);
 	}
 
 	newBoundingBoxAttachment (skin: Skin, name: string): BoundingBoxAttachment {

spine-ts/spine-core/src/IkConstraint.ts
@@ -210,7 +210,7 @@ export class IkConstraint extends Constraint<IkConstraint, IkConstraintData, IkC
 			cwx = a * child.x + b * child.y + parent.worldX;
 			cwy = c * child.x + d * child.y + parent.worldY;
 		}
-		// biome-ignore lint/style/noNonNullAssertion: reference-runtime
+		// biome-ignore lint/style/noNonNullAssertion: reference runtime
 		const pp = parent.bone.parent!.applied;
 		a = pp.a;
 		b = pp.b;

spine-ts/spine-core/src/Skeleton.ts
@@ -462,7 +462,7 @@ export class Skeleton {
 				if (attachment instanceof RegionAttachment) {
 					verticesLength = 8;
 					vertices = Utils.setArraySize(temp, verticesLength, 0);
-					attachment.computeWorldVertices(slot, vertices, 0, 2);
+					attachment.computeWorldVertices(slot, attachment.getOffsets(slot.applied), vertices, 0, 2);
 					triangles = Skeleton.quadTriangles;
 				} else if (attachment instanceof MeshAttachment) {
 					verticesLength = attachment.worldVerticesLength;

commit 8e10e77f5c23dff1876b2a819ddd48ce7577dbdc

[ts][pixi-v8] Fixed clipping after regression introduced in a5ac6e4d.

spine-ts/spine-pixi-v8/src/SpinePipe.ts
@@ -219,7 +219,8 @@ export class SpinePipe implements RenderPipe<Spine> {
 				if (!cacheData.skipRender) {
 					const batchableSpineSlot = gpuSpine.slotBatches[cacheData.id];
 					if (batchableSpineSlot) {
-						batchableSpineSlot.uvs = cacheData.uvs;
+						// biome-ignore lint/style/noNonNullAssertion: if clipped is true, clippedData exists
+						batchableSpineSlot.uvs = cacheData.clipped ? cacheData.clippedData!.uvs : cacheData.uvs;
 						batchableSpineSlot._batcher?.updateElement(batchableSpineSlot);
 					}
 				}