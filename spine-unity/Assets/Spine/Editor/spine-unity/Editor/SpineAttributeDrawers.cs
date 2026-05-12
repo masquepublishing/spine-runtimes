ed44613c0223620ed91b56259a799bb6cfa3e873
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2026-05-12 12:07:01 +0200

    [unity] Minor: restored encoding of original dummy string (less misleading, functionally irrelevant). Closes #3083.

--- spine-unity/Assets/Spine/Editor/spine-unity/Editor/SpineAttributeDrawers.cs
+++ spine-unity/Assets/Spine/Editor/spine-unity/Editor/SpineAttributeDrawers.cs
@@ -202,7 +202,7 @@ namespace Spine.Unity.Editor {
 		protected virtual void HandleSelect (object menuItemObject) {
 			SpineDrawerValuePair clickedItem = (SpineDrawerValuePair)menuItemObject;
 			SerializedProperty serializedProperty = clickedItem.property;
-			if (serializedProperty.serializedObject.isEditingMultipleObjects) serializedProperty.stringValue = "oaifnoiasf��123526"; // HACK: to trigger change on multi-editing.
+			if (serializedProperty.serializedObject.isEditingMultipleObjects) serializedProperty.stringValue = "oaifnoiasf°ñ123526"; // HACK: to trigger change on multi-editing.
 			serializedProperty.stringValue = clickedItem.stringValue;
 			serializedProperty.serializedObject.ApplyModifiedProperties();
 		}
